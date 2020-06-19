96be737584d047136a77987116140b2a7c585394
Brad Traversy <bradtraversy@Brads-Mac-Pro.local>
2020-06-19 09:07:48 -0400

    Delete & single story

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,25 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate('user').lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    res.render('stories/show', {
+      story,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/404')
+  }
+})
+
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -87,6 +106,18 @@ router.put('/:id', ensureAuth, async (req, res) => {
       res.redirect('/dashboard')
     }
   } catch (error) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
+
+// @desc    Delete story
+// @route   DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id })
+    res.redirect('/dashboard')
+  } catch (err) {
     console.error(err)
     return res.render('error/500')
   }
