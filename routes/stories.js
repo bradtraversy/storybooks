a8e2e89b00b4ed498f1a27c50005f7944ef774a7
Brad Traversy <bradtraversy@Brads-Mac-Pro.local>
2020-06-19 09:08:48 -0400

    User stories

--- routes/stories.js
+++ routes/stories.js
@@ -123,4 +123,24 @@ router.delete('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .lean()
+
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 module.exports = router
