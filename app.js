5731cd114665f94895769cb0ecf2db4eb71134b9
Brad Traversy <bradtraversy@Brads-Mac-Pro.local>
2020-06-19 08:43:53 -0400

    Connect to database

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+connectDB()
 
 const app = express()
 
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
