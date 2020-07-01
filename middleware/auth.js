d6f214d709e118ef60912c4a3f6f172b83326541
Kristina Chausheva <52411816+KristinaChausheva@users.noreply.github.com>
2020-07-02 02:44:19 +0300

    Update auth.js
    req object in ensureGuest middleware was null - this is how i fixed it

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,10 +7,10 @@ module.exports = {
     }
   },
   ensureGuest: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      res.redirect('/dashboard')
+    if (!req.isAuthenticated()) {
+      return next();
     } else {
-      return next()
+      res.redirect('/dashboard');
     }
   },
 }
